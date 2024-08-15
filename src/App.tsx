3a3edda031ce79c2cbe9d8f4c57afd1fc1e2575e
Luca <lucabohneberger@gmail.com>
2024-08-15 14:49:26 -0300

    responsividade e mais sites

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,19 @@ function App() {
 
   return (
     <div className='page'>
-      <div style={{ display: 'flex', gap: '5rem' }} >
+      <div className='page2' >
         <div style={{ display: 'flex', gap: '1rem', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
           <h3 style={{ fontSize: '1rem' }} >Links úteis</h3>
           <Button text="Todos os planos de aula/Ementas" link="https://www.politecnica.pucrs.br/programas/index.php" />
           <Button text="Biblioteca" link="https://www.pucrs.br/biblioteca/" />
           <Button text="Moodle" link="https://moodle.pucrs.br/" />
-          <Button text="Intranet?" link="https://intranet.pucrs.br/" />
           <Button text="Trabalho de Conclusão de Curso" link="https://www.politecnica.pucrs.br/conclusao/consulta/index.php" />
+          <Button text="Moodle de outros semestres" link="https://moodle.pucrs.br/" />
+          <Button text="Disciplinas Online" link="https://moodle.pucrs.br/cead/disciplinas_online//" />
+          <Button text="Ver laboratórios" link="https://sarc.pucrs.br/Default/" />
         </div>
         <div style={{ display: 'flex', gap: '1rem', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-          <h3 style={{ fontSize: '1rem' }} > Informções detalhadas do curso</h3>
+          <h3 style={{ fontSize: '1rem' }} > Informações detalhadas do curso</h3>
           <Button text="Engenharia de Software" link="https://www.pucrs.br/grade-corpo-docente/dados-do-curso/?slug=engenharia-de-software" />
           <Button text="Engenharia de Computação" link="https://www.pucrs.br/grade-corpo-docente/dados-do-curso/?slug=engenharia-de-computacao" />
           <Button text="Engenharia de Controle e Automação" link="https://www.pucrs.br/grade-corpo-docente/dados-do-curso/?slug=engenharia-de-controle-e-automacao" />
@@ -29,11 +31,15 @@ function App() {
           <Button text="Sistemas de Informação" link="https://www.pucrs.br/grade-corpo-docente/dados-do-curso/?slug=sistemas-de-informacao" />
           <Button text="Ciência de Dados" link="https://www.pucrs.br/grade-corpo-docente/dados-do-curso/?slug=ciencia-de-dados" />
         </div>
+        <div style={{ display: 'flex', gap: '1rem', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+          <h3 style={{ fontSize: '1rem' }} >Links provavelmente inuteis</h3>
+          <Button text="Intranet?" link="https://intranet.pucrs.br/" />
+        </div>
       </div>
-      <div style={{ display: 'flex', alignItems: 'flex-end', justifyContent: 'flex-end', flexDirection: 'column' }}>
-        <div style={{display: 'flex', flexDirection: 'column'}}>
-          <p style={{ display: 'flex' , margin: '0'}} >Site por Luca W. Bohnenberger</p>
-          <input style={{backgroundColor: '#353535', border: '0', color: 'white'}} type="button" value="Contato" onClick={() => window.open('https://github.com/LucaWBohnenberger')} />
+      <div className="rodape">
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
+          <p style={{ display: 'flex', margin: '0' }} >Site por Luca W. Bohnenberger</p>
+          <input style={{ backgroundColor: '#353535', border: '0', color: 'white' }} type="button" value="Contato" onClick={() => window.open('https://github.com/LucaWBohnenberger')} />
         </div>
       </div>
     </div>
